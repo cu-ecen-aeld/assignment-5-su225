9c880da21b3fee4eb8676678c77ae5a79de76c97
Suchith.J.N <suchithjn22@gmail.com>
2023-09-16 13:49:30 +0530

    finish assignment-4-buildroot

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'cab78939cdc2e1cba5dfeac4eeaa41dadbe39a17'
+AESD_ASSIGNMENTS_VERSION = '355574d2b6c8bf916a48cc85ea8c2385f5bbebd5'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
